4f488e4c360bdef918986f049e4fdbf3635bf3e8
zongyanqi <zongyanqi@foxmail.com>
2017-03-19 22:34:16 +0800

    add Easy_504_Base_7

---
+++ Easy_504_Base_7.js
@@ -0,0 +1,38 @@
+/**
+ *
+ * Given an integer, return its base 7 string representation.
+
+ Example 1:
+ Input: 100
+ Output: "202"
+
+ Example 2:
+ Input: -7
+ Output: "-10"
+
+ Note: The input will be in range of [-1e7, 1e7].
+
+ */
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function (num) {
+
+    if (num === 0) return '0';
+    var str = '';
+    var sign = num > 0;
+    num = Math.abs(num);
+    while (num) {
+        str = (num % 7) + str;
+        num = Math.floor(num / 7);
+    }
+
+    return sign ? str : '-' + str;
+
+};
+
+console.log(convertToBase7(0));
+console.log(convertToBase7(100));
+console.log(convertToBase7(-7));
