86fa4fe3536d72af4bc6dd7845aea93f75dc3941
zongyanqi <zongyanqi@foxmail.com>
2017-03-19 16:04:55 +0800

    add Easy_1_Two_Sum.js Easy_167_Two_Sum_II_Input_array_is_sorted

---
+++ Easy_167_Two_Sum_II_Input_array_is_sorted.js
@@ -0,0 +1,29 @@
+/**
+ * Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+ The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+ You may assume that each input would have exactly one solution and you may not use the same element twice.
+
+ Input: numbers=[2, 7, 11, 15], target=9
+ Output: index1=1, index2=2
+
+
+ */
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+
+    for (var i = 0; i < numbers.length - 1; i++) {
+        for (var j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] == target) return [i + 1, j + 1];
+        }
+    }
+};
+
+
+console.log(twoSum([2, 7, 11, 15], 9))
---
+++ Easy_1_Two_Sum.js
@@ -0,0 +1,25 @@
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+ Example:
+ Given nums = [2, 7, 11, 15], target = 9,
+
+ Because nums[0] + nums[1] = 2 + 7 = 9,
+ return [0, 1].
+ */
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+
+    for (var i = 0; i < numbers.length - 1; i++) {
+        for (var j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] == target) return [i, j];
+        }
+    }
+};
