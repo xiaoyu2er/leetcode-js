63ae02fd47695f3434c2121a3453917d247026b3
zongyanqi <zongyanqi@foxmail.com>
2017-03-19 10:03:28 +0800

    add Easy_136_Single_Number Easy_190_Reverse_Bits Easy_191_Number_of_1_Bits

---
+++ Easy_136_Single_Number.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * https://leetcode.com/problems/single-number/#/description
+ *
+ * Given an array of integers, every element appears twice except for one. Find that single one.
+ *
+ * Note:
+ * Your algorithm should have a linear runtime complexity.
+ *
+ * Could you implement it without using extra memory?
+ *
+ *
+ * https://discuss.leetcode.com/topic/1916/my-o-n-solution-using-xor/1
+ * Hint: known that A XOR A = 0 and the XOR operator is commutative
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+
+    return nums.reduce((ret, n) => ret ^= n, 0);
+
+};
+
+console.log(singleNumber([1, 2, 2]) == 1);
+console.log(singleNumber([1, 2, 1]) == 2);
---
+++ Easy_190_Reverse_Bits.js
@@ -0,0 +1,30 @@
+/**
+ * https://leetcode.com/problems/reverse-bits/#/description
+ *
+ * Reverse bits of a given 32 bits unsigned integer.
+ *
+ * For example, given input 43261596 (represented in binary as 00000010100101000001111010011100),
+ *
+ * return 964176192 (represented in binary as 00111001011110000010100101000000).
+ *
+ * 00000010100101000001111010011100
+ * 00111001011110000010100101000000
+ */
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function (n) {
+    // reverse binary str
+    var str = '';
+    var i = 32;
+    while (i--) {
+        str += n % 2;
+        n = Math.floor(n / 2);
+    }
+
+    return parseInt(str, 2);
+};
+
+console.log(reverseBits(43261596) == 964176192);
